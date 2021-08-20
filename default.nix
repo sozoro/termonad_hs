e5d1bdae32463f5d20c25b7d0ff94254d8b0d3a7
sozoro <46480375+sozoro@users.noreply.github.com>
2021-08-21 04:16:47 +0900

    add cmdName variable

--- default.nix
+++ default.nix
@@ -3,17 +3,19 @@
 # reference: https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/x11/window-managers/xmonad.nix
 
 let
+  cmdName     = "termonad";
+  packageName = cmdName;
   packages    = self: [ self.termonad self.optparse-applicative ];
   termonadEnv = haskellPackages.ghcWithPackages packages;
-  configured  = writers.writeHaskellBin "termonad" {
+  configured  = writers.writeHaskellBin cmdName {
     ghc       = haskellPackages.ghc;
     libraries = packages haskellPackages;
     ghcArgs   = [];
   } ./termonad.hs;
 in
-  runCommandLocal "termonad" {
+  runCommandLocal packageName {
     nativeBuildInputs = [ makeWrapper termonadEnv ];
   } ''
-    makeWrapper ${configured}/bin/termonad $out/bin/termonad \
+    makeWrapper ${configured}/bin/${cmdName} $out/bin/${cmdName} \
       --set NIX_GHC "${termonadEnv}/bin/ghc"
   ''
